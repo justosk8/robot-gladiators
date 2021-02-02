dfcdae794a276aeb568dd58fc9e29a36f869c73c
Ed Abbey <websterhomer78@gmail.com>
2021-02-01 21:39:29 -0700

    multiple rounds complete

--- assets/js/game.js
+++ assets/js/game.js
@@ -70,13 +70,27 @@ var fight = function(enemyName) {
         break;
       } else {
         window.alert(playerName + ' still has ' + playerHealth + ' health left.');
-      }
+      } 
+      
     }
   };
 
 // run fight function to start game
 for(var i = 0; i < enemyNames.length; i++) {
+    if (playerHealth > 0) {
+        window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+        // pick new enemy to fight based on the index of the enemyNames array
     var pickedEnemyName = enemyNames[i];
+    // reset eneyHealth before starting new fight
     enemyHealth = 50;
+    // use debugger to pause script from running and check what's going on at that moment with the code
+     debugger;
+
+    // pass the pickedEnemyName variable's value into the fight function, where it wekk assume the value of the enemyName parameter
     fight(pickedEnemyName);
-}
+    }
+    // if player isn't alive, stop the game
+  else {
+    window.alert('You have lost your robot in battle! Game Over!');
+  }
+} 
