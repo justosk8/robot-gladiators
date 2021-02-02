4f63daf37dbf78e232d0a898ec267568ed69d514
Ed Abbey <websterhomer78@gmail.com>
2021-02-02 16:07:35 -0700

    snapshot starting point

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,23 +1,18 @@
-// Game States
-// "WIN" - Player robot has defeated all enemy-robots
-//    * Fight all enemy-robots
-//    * Defeat each enemy-robot
-// "LOSE" - Player robot's health is zero or less
-// var playerName = 'Clank McKrank';
 var playerName = window.prompt("What is your robot's name?");
 var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-
-
 var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+console.log(enemyNames);
+console.log(enemyNames.length);
+console.log(enemyNames[0]);
+console.log(enemyNames[3]);
 
-
-// fight function
+// fight function (now with parameter for enemy's name)
 var fight = function(enemyName) {
     while (playerHealth > 0 && enemyHealth > 0) {
       // ask player if they'd liked to fight or run
@@ -33,7 +28,7 @@ var fight = function(enemyName) {
           window.alert(playerName + ' has decided to skip this fight. Goodbye!');
           // subtract money from playerMoney for skipping
           playerMoney = playerMoney - 10;
-          console.log("playerMoney", playerMoney)
+          
           break;
         }
       }
@@ -75,14 +70,19 @@ var fight = function(enemyName) {
     }
   };
 
-// run fight function to start game
+// fight each enemy-robot by looping over them and fighting them one at a time
+
 for(var i = 0; i < enemyNames.length; i++) {
+  // if player is still alive, keep fighting
     if (playerHealth > 0) {
+      // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
         window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-        // pick new enemy to fight based on the index of the enemyNames array
+
+    // pick new enemy to fight based on the index of the enemyNames array
     var pickedEnemyName = enemyNames[i];
     // reset eneyHealth before starting new fight
     enemyHealth = 50;
+    
     // use debugger to pause script from running and check what's going on at that moment with the code
      debugger;
 
